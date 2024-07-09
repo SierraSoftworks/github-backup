8aa7bdfa9dc619ef6ecb6d86d6f564b993e6c4b2
Benjamin Pannell <benjamin@pannell.dev>
2024-07-10 00:00:58 +0100

    fix: Add SystemConfiguration Apple package building on Darwin

--- flake.nix
+++ flake.nix
@@ -43,6 +43,7 @@
           ] ++ lib.optionals pkgs.stdenv.isDarwin [
             # Additional darwin specific inputs can be set here
             pkgs.libiconv
+            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
           ];
 
           # Additional environment variables can be set directly
