e8235a7694088d65bce5383776227a798dfc4ee6
Benjamin Pannell <benjamin@pannell.dev>
2024-08-08 00:30:34 +0100

    test: Add tests for formatting of location info

--- src/filter/location.rs
+++ src/filter/location.rs
@@ -14,6 +14,27 @@ impl Loc {
 
 impl Display for Loc {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "line {}, column {}", self.line, self.column)
+        match self {
+            Loc { line: 0, column: 0 } => {
+                write!(f, "unknown location")
+            }
+            Loc { line, column } => {
+                write!(f, "line {}, column {}", line, column)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case(0, 0, "unknown location")]
+    #[case(1, 2, "line 1, column 2")]
+    fn test_display(#[case] line: usize, #[case] column: usize, #[case] expected: &str) {
+        assert_eq!(format!("{}", Loc::new(line, column)), expected);
     }
 }
